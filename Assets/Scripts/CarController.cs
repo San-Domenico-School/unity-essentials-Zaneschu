9c9a83004c59fb123ef02826c4d7486546cea2ea
Zaneschu <zschuman27@sandomenico.org>
2024-04-26 12:32:23 -0700

    car

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        userInput();
+        Move();
+    }
+
+    // This method get WASD and Arrow keys from user and sets direction magnitudes
+    void userInput()
+    {
+
+    }
+
+    // This method uses direction magnitudes to move the attached object
+    private void Move()
+    {
+        
+    }
+}
